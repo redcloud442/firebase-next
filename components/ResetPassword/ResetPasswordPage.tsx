7f51b92243c73396774f38f78b3d7576df173355
redcloud442 <cloudred442@gmail.com>
2025-04-27 20:19:19 +0800

    newest commit

--- components/ResetPassword/ResetPasswordPage.tsx
+++ components/ResetPassword/ResetPasswordPage.tsx
@@ -67,7 +67,7 @@ const ResetPasswordPage = () => {
     <div className="flex items-center justify-center min-h-screen px-4">
       <nav className="absolute top-0 left-0 w-full min-h-28 bg-black/20 z-50 flex items-center justify-center">
         <h1 className="text-3xl sm:text-5xl font-black tracking-wide text-outline italic">
-          RoadWheelLearn
+          RoadWheeLearn
         </h1>
       </nav>
       <Image
